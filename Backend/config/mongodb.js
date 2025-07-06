f89a629c4b15b26765e300fa19db9fe1e09888b2
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-07-07 03:14:00 +0530

    fix: Update MongoDB connection URI to use 'E-Cell' for improved clarity

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -5,7 +5,7 @@ const connectDB = async () =>{
     mongoose.connection.on("connected", () => {
         console.log("MongoDB connected successfully");
     });
-    await mongoose.connect(`${process.env.MONGODB_URI}/ecellsmvit`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/E-Cell`);
 }
 
 export default connectDB;

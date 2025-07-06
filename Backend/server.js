fbc2c4cf3e9d010b3bdc8dab89d7a2ed9e44b9c2
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-07-06 15:55:49 +0530

    db connected

---
+++ Backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import { connectDB } from "./db/connectDB.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+app.use(express.json()); 
+app.use(cookieParser()); 
+
+
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server is running on port no: ", PORT);
+});

5451471890fecfd844fc8b85cc7b37ab8e012f05
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-07-07 13:28:02 +0530

    fix: Improve MongoDB connection error handling and ensure successful connection logging

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
-const connectDB = async () =>{
-
-    mongoose.connection.on("connected", () => {
+const connectDB = async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/ecellsmvit`);
         console.log("MongoDB connected successfully");
-    });
-    await mongoose.connect(`${process.env.MONGODB_URI}/ecellsmvit`);
-}
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        process.exit(1);
+    }
+};
 
 export default connectDB;

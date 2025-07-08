a6a6da3679a6aa154eec70b09f5b1b123f606686
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-07-08 19:27:04 +0530

    refactor: Remove user authentication middleware from OTP verification route
    - Simplified the '/send-verify-otp' route by removing the user authentication middleware, allowing direct access to the OTP sending process.
    - This change streamlines the OTP verification flow and enhances the user experience during account verification.

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -7,7 +7,7 @@ const authRouter = express.Router();
 authRouter.post('/register', register);
 authRouter.post('/login', login);
 authRouter.post('/logout', logout);
-authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp);
+authRouter.post('/send-verify-otp', sendVerifyOtp);
 authRouter.post('/verify-account', verifyEmail);
 authRouter.get('/is-auth', userAuth, isAuthenticated);
 

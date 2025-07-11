bba82c15ca468991bddf7e6d5b15807b6c558ec2
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-07-12 01:11:07 +0530

    fix(userAuth): update token extraction to use 'token' cookie
    - Changed the token extraction method in userAuth middleware from req.cookies.accessToken to req.cookies.token for consistency.
    - Updated allowed origins in server.js to remove trailing slash from localhost URL.

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const app = express()
 const PORT = process.env.PORT || 4000
 const allowedOrigins = [
     'https://e-cell-smvit.onrender.com',
-    'http://localhost:5173/'
+    'http://localhost:5173'
 ]
 
 // Connect to the database

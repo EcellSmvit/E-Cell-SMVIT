d46017f9673f97be98027cc96c461dab0954d8ed
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-07-16 13:47:04 +0530

    feat(main): integrate React Query for data fetching
    - Added QueryClient and QueryClientProvider to enable React Query for efficient data management.
    - Wrapped the application in StrictMode to enhance development practices and identify potential issues.
    - This change improves the application's data handling capabilities and prepares it for future enhancements.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AppContextProvider } from './context/AppContext.jsx'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+// Create the query client
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <BrowserRouter>
-      <AppContextProvider>
-        <App />
-      </AppContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
+      </QueryClientProvider>
     </BrowserRouter>
-);
+  </StrictMode>
+)

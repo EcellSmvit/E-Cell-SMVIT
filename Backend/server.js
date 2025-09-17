9cc82a3944da90b9180617fe4d95cd259e1642af
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-09-17 22:47:02 +0530

    refactor(server.js): update CORS configuration to include additional allowed origins for improved access control

--- Backend/server.js
+++ Backend/server.js
@@ -21,22 +21,26 @@ const PORT = process.env.PORT || 4000
 
 connectDB()
 
-// ✅ Define corsOptions first
-const allowedOrigins = ['https://e-cell-smvit.onrender.com']
+
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true)
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true)
-    } else {
-      return callback(new Error('Not allowed by CORS'))
-    }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  optionsSuccessStatus: 200,
-}
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true)
+  
+      const allowed = [
+        'https://e-cell-smvit.onrender.com',
+        'https://www.ecellsmvit.in',
+        'https://ecellsmvit.in'
+      ]
+  
+      if (allowed.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    credentials: true,
+  }
+  
 
 // ✅ Now use it
 app.use(cors(corsOptions))

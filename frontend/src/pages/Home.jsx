b0c269fe0e0b2de49457f4e79f9e97af139877dc
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-09-30 21:55:59 +0530

    delete(frontend): remove unused PublicSection components to streamline codebase and improve maintainability

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,29 +3,18 @@ import React from 'react'
 // import About from '../components/PublicSection/About'
 // import Background from '../components/PublicSection/Background';
 import Navbar from '../components/PublicSection/Navbar'
-// import Particles from './components/Particlesbackground';
-// import TextHome from '../components/PublicSection/TextHome';
 import ThreeScene from '../components/PublicSection/ThreeScene';
 import Section2 from '../components/PublicSection/Section2';
-// import AchievementSection from '../components/PublicSection/AchievementSection';
-import Section5 from '../components/PublicSection/Section5';
 import Section6 from '../components/PublicSection/Section6';
-// import ImageSection from '@/components/PublicSection/Image';
 import Text from '@/components/PublicSection/Text';
 import InfinityComponent from '@/components/PublicSection/Infinity';
 import Footer from '@/components/PublicSection/Footer';
-import AchievementMobile from '@/components/ResponsiveComponent/AchievementMobile';
-import AboutMobile from '@/components/ResponsiveComponent/AboutMobile';
-// import Gallery from '@/components/PublicSection/Gallery';
-// import Events from '@/components/PublicSection/Events';
 import Heads from '@/components/PublicSection/Heads';
-// import Aboutsectiontesting from '@/components/Aboutsectiontesting';
 import StartupBacked from '@/components/PublicSection/StartupBacked';
 import OurMissionandVision from '@/components/PublicSection/OurMissionandVision';
 import WhyEcell from '@/components/PublicSection/WhyEcell';
 import Stepperform from './Stepperform';
 import { SignedIn } from '@clerk/clerk-react';
-// import Recurtiment from '@/components/EventsFormHandling/Recurtiment';
 
 
 
@@ -35,42 +24,15 @@ function Home() {
       <div style={{ position: "fixed", top: 0, left: 0, width: "100%", zIndex: 50 }}>
         <Navbar />
       </div>
-      {/* <TextHome/> */}
-      {/* <Background/> */}
-      
       <ThreeScene />
-      
       <InfinityComponent/>
       <Text/>
       <Section2/>
-      
-      {/* <Gallery/> */}
-      {/* <div className="hidden sm:block">
-        <ImageSection/>
-      </div> */}
-      
-      <div className="block sm:hidden">
-        <AboutMobile/>
-      </div>
-      
-      <div className="hidden sm:block">
-        {/* <AchievementSection/> */}
-      </div>
-      <div className="block sm:hidden">
-        <AchievementMobile/>
-      </div>
-      {/* <Section5/> */}
       <OurMissionandVision/>
       <StartupBacked/>
       <Heads/>
-      {/* <Aboutsectiontesting/> */}
-      {/* <About/> */}
-      
-      {/* <Events/> */}
       <WhyEcell/>
       <Section6/>
-      {/* <Recurtiment/> */}
-      
       <Footer/>
       <SignedIn>
         <Stepperform/>

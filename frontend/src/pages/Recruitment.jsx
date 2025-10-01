34055816b45b28ce927677124caf4feaff6da237
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-10-01 23:38:18 +0530

    style(Recruitment): update text color to black for improved readability and consistency across form fields and sections

--- frontend/src/pages/Recruitment.jsx
+++ frontend/src/pages/Recruitment.jsx
@@ -137,36 +137,36 @@ function Recruitment() {
                 {/* Step 1 */}
                 <Step>
                   <h2 className='text-2xl font-bold text-[#5227FF]'>Why E-CELL SMVIT?</h2>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor neque vel veritatis mollitia officiis quae! Maxime eius totam obcaecati atque nostrum soluta harum quos repellendus quis itaque temporibus neque, culpa inventore dolore cumque error expedita autem quod rerum at? Excepturi, incidunt magnam. Pariatur architecto ipsa molestiae neque cumque placeat minus corrupti asperiores dolores, iste assumenda vitae esse explicabo fugiat cum nulla reiciendis eligendi culpa reprehenderit quo itaque labore ipsam. Cum iusto minima aspernatur voluptatem perferendis laudantium nulla distinctio eveniet! Nostrum sint saepe blanditiis quisquam magnam nulla ipsum quas voluptates amet, reiciendis perferendis tenetur facere, atque et natus cupiditate qui aspernatur.</p>
+                  <p className='text-black'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor neque vel veritatis mollitia officiis quae! Maxime eius totam obcaecati atque nostrum soluta harum quos repellendus quis itaque temporibus neque, culpa inventore dolore cumque error expedita autem quod rerum at? Excepturi, incidunt magnam. Pariatur architecto ipsa molestiae neque cumque placeat minus corrupti asperiores dolores, iste assumenda vitae esse explicabo fugiat cum nulla reiciendis eligendi culpa reprehenderit quo itaque labore ipsam. Cum iusto minima aspernatur voluptatem perferendis laudantium nulla distinctio eveniet! Nostrum sint saepe blanditiis quisquam magnam nulla ipsum quas voluptates amet, reiciendis perferendis tenetur facere, atque et natus cupiditate qui aspernatur.</p>
                 </Step>
-                <Step>
+                <Step >
                   <h2 className='text-2xl font-bold text-[#5227FF] mb-4 text-center'>Team Roles</h2>
                   <div className='grid grid-cols-1 gap-6 justify-items-center w-full sm:grid-cols-2 md:grid-cols-3 md:gap-8'>
                     <ul className="p-4 w-full max-w-xs">
                       <li className="font-bold text-[#5227FF] mb-2">Operational Manager</li>
-                      <li className="text-white">- Arranging Logistics and Permissions</li>
-                      <li className="text-white">- Documenting operations and events</li>
+                      <li className="text-black">- Arranging Logistics and Permissions</li>
+                      <li className="text-black">- Documenting operations and events</li>
                     </ul>
                     <ul className="p-4 w-full max-w-xs">
                       <li className="font-bold text-[#5227FF] mb-2">Events & PR Manager</li>
-                      <li className="text-white">- Conducting of Events</li>
-                      <li className="text-white">- Gathering and Influencing People</li>
+                      <li className="text-black">- Conducting of Events</li>
+                      <li className="text-black">- Gathering and Influencing People</li>
                     </ul>
                     <ul className="p-4 w-full max-w-xs">
                       <li className="font-bold text-[#5227FF] mb-2">Corporate Relations Manager</li>
-                      <li className="text-white">- Arranging Sponsorships</li>
-                      <li className="text-white">- Making funding sources from companies</li>
+                      <li className="text-black">- Arranging Sponsorships</li>
+                      <li className="text-black">- Making funding sources from companies</li>
                     </ul>
                     <ul className="p-4 w-full max-w-xs">
                       <li className="font-bold text-[#5227FF] mb-2">Marketing Manager</li>
-                      <li className="text-white">- Advertising and Marketing Events</li>
-                      <li className="text-white">- Influencing Students</li>
+                      <li className="text-black">- Advertising and Marketing Events</li>
+                      <li className="text-black">- Influencing Students</li>
                     </ul>
                     <ul className="p-4 w-full max-w-xs">
                       <li className="font-bold text-[#5227FF] mb-2">Design Media Manager</li>
-                      <li className="text-white">- Posting and planning Media</li>
-                      <li className="text-white">- Social Media Growth Strategy</li>
-                      <li className="text-white">- Designing Social Content</li>
+                      <li className="text-black">- Posting and planning Media</li>
+                      <li className="text-black">- Social Media Growth Strategy</li>
+                      <li className="text-black">- Designing Social Content</li>
                     </ul>
                   </div>
                 </Step>
@@ -178,24 +178,24 @@ function Recruitment() {
                     {/* Name, Year, USN, Gender */}
                     <div className="flex flex-col gap-4 md:flex-row md:gap-4">
                       <div className="flex-1">
-                        <label className="block mb-1 font-semibold text-white" htmlFor="name">Name</label>
+                        <label className="block mb-1 font-semibold text-black" htmlFor="name">Name</label>
                         <input
                           id="name"
                           type="text"
                           value={name}
                           onChange={(e) => setName(e.target.value)}
                           placeholder="Your name"
-                          className="px-3 py-2 w-full text-white bg-gray-800 rounded"
+                          className="px-3 py-2 w-full text-black bg-gray-400 rounded"
                           autoComplete="off"
                         />
                       </div>
                       <div className="flex-1">
-                        <label className="block mb-1 font-semibold text-white" htmlFor="year">Year</label>
+                        <label className="block mb-1 font-semibold text-black" htmlFor="year">Year</label>
                         <select
                           id="year"
                           value={year}
                           onChange={(e) => setYear(e.target.value)}
-                          className="px-3 py-2 w-full text-white bg-gray-800 rounded"
+                          className="px-3 py-2 w-full text-black bg-gray-400 rounded"
                           autoComplete="off"
                         >
                           <option value="">Select Year</option>
@@ -211,14 +211,14 @@ function Recruitment() {
                           value={usn}
                           onChange={(e) => setUsn(e.target.value)}
                           placeholder="Your USN"
-                          className="px-3 py-2 w-full text-white bg-gray-800 rounded"
+                          className="px-3 py-2 w-full text-black bg-gray-400 rounded"
                           autoComplete="off"
                         />
                       </div>
                       <div className="flex-1">
-                        <label className="block mb-1 font-semibold text-white">Gender</label>
+                        <label className="block mb-1 font-semibold text-black">Gender</label>
                         <div className="flex gap-4">
-                          <label className="flex gap-1 items-center text-white">
+                          <label className="flex gap-1 items-center text-black">
                             <input
                               type="radio"
                               name="gender"
@@ -228,7 +228,7 @@ function Recruitment() {
                             />
                             Male
                           </label>
-                          <label className="flex gap-1 items-center text-white">
+                          <label className="flex gap-1 items-center text-black">
                             <input
                               type="radio"
                               name="gender"
@@ -245,7 +245,7 @@ function Recruitment() {
                     {/* Questions */}
                     <div className="flex flex-col gap-4 md:flex-row md:gap-4">
                       <div className="flex-1">
-                        <label className="block mb-1 font-semibold text-white" htmlFor="q1">
+                        <label className="block mb-1 font-semibold text-black" htmlFor="q1">
                           Why do you want to join the E-Cell, and what do you hope to contribute to our entrepreneurial community?
                         </label>
                         <textarea
@@ -253,13 +253,13 @@ function Recruitment() {
                           value={q1}
                           onChange={(e) => setQ1(e.target.value)}
                           placeholder="Your answer"
-                          className="px-3 py-2 w-full text-white bg-gray-800 rounded"
+                          className="px-3 py-2 w-full text-black bg-gray-400 rounded"
                           rows={3}
                           autoComplete="off"
                         />
                       </div>
                       <div className="flex-1">
-                        <label className="block mb-1 font-semibold text-white" htmlFor="q2">
+                        <label className="block mb-1 font-semibold text-black" htmlFor="q2">
                           Do you have any prior experience in startups, entrepreneurship, or event management? Please elaborate.
                         </label>
                         <textarea
@@ -267,7 +267,7 @@ function Recruitment() {
                           value={q2}
                           onChange={(e) => setQ2(e.target.value)}
                           placeholder="Your answer"
-                          className="px-3 py-2 w-full text-white bg-gray-800 rounded"
+                          className="px-3 py-2 w-full text-black bg-gray-400 rounded"
                           rows={3}
                           autoComplete="off"
                         />
@@ -276,7 +276,7 @@ function Recruitment() {
 
                     <div className="flex flex-col gap-4 md:flex-row md:gap-4">
                       <div className="flex-1">
-                        <label className="block mb-1 font-semibold text-white" htmlFor="q3">
+                        <label className="block mb-1 font-semibold text-black" htmlFor="q3">
                           Describe a situation where you faced a challenge in a team project or initiative. How did you handle it, and what was the outcome?
                         </label>
                         <textarea
@@ -284,13 +284,13 @@ function Recruitment() {
                           value={q3}
                           onChange={(e) => setQ3(e.target.value)}
                           placeholder="Your answer"
-                          className="px-3 py-2 w-full text-white bg-gray-800 rounded"
+                          className="px-3 py-2 w-full text-black bg-gray-400 rounded"
                           rows={3}
                           autoComplete="off"
                         />
                       </div>
                       <div className="flex-1">
-                        <label className="block mb-1 font-semibold text-white" htmlFor="q4">
+                        <label className="block mb-1 font-semibold text-black" htmlFor="q4">
                           Share an innovative idea or project you have worked on. How did you execute it, and what impact did it have?
                         </label>
                         <textarea
@@ -298,7 +298,7 @@ function Recruitment() {
                           value={q4}
                           onChange={(e) => setQ4(e.target.value)}
                           placeholder="Your answer"
-                          className="px-3 py-2 w-full text-white bg-gray-800 rounded"
+                          className="px-3 py-2 w-full text-black bg-gray-400 rounded"
                           rows={3}
                           autoComplete="off"
                         />
@@ -312,18 +312,18 @@ function Recruitment() {
                   <h2 className="mb-2 text-2xl font-bold">Final Step</h2>
                   <p className="mb-4">You made it!</p>
                   <div className="mb-4">
-                    <p className="text-white">
+                    <p className="text-black">
                       Please review your answers before submitting your application. Make sure all information is accurate and complete.
                     </p>
-                    <ul className="mt-2 list-disc list-inside text-gray-300">
+                    <ul className="mt-2 list-disc list-inside text-gray-800">
                       <li>Double-check your contact details.</li>
                       <li>Ensure your answers reflect your true experiences and aspirations.</li>
                       <li>Once you submit, you will not be able to edit your responses.</li>
                     </ul>
                   </div>
                   <div className="mb-4">
-                    <p className="font-semibold text-white">What happens next?</p>
-                    <p className="text-gray-300">
+                    <p className="font-semibold text-black">What happens next?</p>
+                    <p className="text-gray-800">
                       Our team will review your application and contact you via email regarding the next steps. Keep an eye on your inbox!
                     </p>
                   </div>

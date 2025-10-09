04f1778babe0720ec318e0ebf4cef76335adbbde
Bikesh Kumar <iambikeshkumar07@gmail.com>
2025-10-09 21:02:13 +0530

    refactor(Recruitment): remove Google Sheets API integration from form submission to simplify data handling

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -35,16 +35,7 @@ export const submitApplication = async (formData) => {
       COLLECTION_ID,
       ID.unique(),
       formData
-    );
-    const GOOGLE_SHEET_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbynAyaCck-IwmX2r_MbkvXzAdD7uMoaqSA95WnF8TQIEuNcd40CJ2vApv6pCAsdR7mS/exec";
-
-    await fetch(GOOGLE_SHEET_WEBAPP_URL, {
-      method: "POST",
-      mode: "no-cors",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(formData),
-    });
-    
+    );    
     console.log("Form submitted successfully", response);
     return response;
   } catch (error) {
